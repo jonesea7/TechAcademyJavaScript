ccaf482abd96f9f77585cd37df596e69657ecde0
eddywithay <edmundalyn@gmail.com>
2020-07-23 09:02:16 -0400

    Add practice file.

---
+++ jS/JavaScript_1.js
@@ -0,0 +1,13 @@
+window.alert("Hello World!")
+
+document.write("Hello World!")
+
+var codingGreeting = "Hello World!"
+
+document.write(codingGreeting)
+
+
+var createAlertBox = "Ahh, Real Monsters!"
+window.alert(createAlertBox)
+
+document.write("Whaddup tho\'")
